3d82000571736cebeda547c9b3b687c8f2782401
RodriMontes <rodrii.montesrms@gmail.com>
2025-03-28 18:33:56 -0300

    actividad 1 y 2

---
+++ 02 Trabajo Colaborativo/import.py
@@ -0,0 +1,12 @@
+import math as mt 
+
+
+
+print(mt.sqrt(81))
+
+from colorama import Fore, Back, Style
+print(Fore.RED + 'some red text')
+print(Back.GREEN + 'and with a green background')
+print(Style.DIM + 'and in dim text')
+print(Style.RESET_ALL)
+print('back to normal now')
